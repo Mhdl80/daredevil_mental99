8feaf844ed7a938cec93ecb44dc56aa5892e4608
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-07-09 20:23:24 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {107098884,180099603,tonumber(our_id)},--Sudo users
+    sudo_users = {107098884,180099603},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
